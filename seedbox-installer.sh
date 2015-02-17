46785f9bba36dc3dfeaf2181ef4877c25f706aea
Jason McEachen <github@brightshinyobject.com>
2015-02-17 10:46:27 -0500

    Added options to specify an alternate SSH port, and to tighten down the firewall rules to the bare minimum.

--- seedbox-installer.sh
+++ seedbox-installer.sh
@@ -19,6 +19,10 @@ ADD_SNMPD=false
 SNMPD_COMMUNITY=public
 SNMPD_REMOTE_IP=localhost
 
+## In case you like to run SSH on an alternate port
+SSH_PORT=22
+
+
 # Update to current.
 apt-get update -y
 apt-get upgrade -y
@@ -74,9 +78,19 @@ if [ $ADD_SNMPD -eq true ]; then
   echo "$SNMPD_REMOTE_IP # SNMPD server" >> /etc/csf/csf.allow
 fi
 
+## Minimize our firewall holes to the bare minimum.
+sed -i 's/^TCP_IN =.*/TCP_IN = "$SSH_PORT,$TRANSMISSION_PORT,51413"/' csf.conf
+sed -i 's/^TCP_OUT =.*/TCP_OUT = "1:65535"/' csf.conf
+## Include SNMP ports if that service has been added.
+if [ $ADD_SNMPD -eq true ]; then
+  sed -i 's/^UDP_IN =.*/UDP_IN = "161,162,51413"/' csf.conf
+else
+  sed -i 's/^UDP_IN =.*/UDP_IN = "51413"/' csf.conf
+fi
+sed -i 's/^UDP_OUT =.*/UDP_OUT = "1:65535"/' csf.conf
+
 echo "Change CSF config TESTING=1 and start the service for 5 minutes."
 echo "Once your config makes you happy, set TESTING=0 and restart to keep it running."
-
 
 
 # Install snmpd if ADD_SNMPD was changed to true.
@@ -85,3 +99,6 @@ if [ $ADD_SNMPD -eq true ]; then
   echo "rocommunity $SNMPD_COMMUNITY $SNMPD_REMOTE_IP" >> /etc/snmp/snmpd.conf
   service snmpd restart
 fi
+
+## Change SSH port if requested
+sed -i 's/^Port .*/Port $SSH_PORT/' /etc/ssh/sshd_config
